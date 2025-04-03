d3c43a3b5ed37fb83700bce6120d82c7c0ad212c
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-04-03 20:12:35 +0500

    Update ansible.tf

--- src/ansible.tf
+++ src/ansible.tf
@@ -1,6 +1,8 @@
-resource “local_file” “hosts_cfg” {
-content = templatefile(“path.module/hosts.tftpl",webservers=yandex 
-compute instance.webserversdatabases=yandex 
-compute instance.databasesstorage=yandex 
-compute instance.storage)filename="{abspath(path.module)}/hosts.cfg”
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/hosts.tftpl",
+   {webservers =  yandex_compute_instance.web
+    databases = yandex_compute_instance.for_each
+    storage = [yandex_compute_instance.storage]}
+  )
+  filename = "${abspath(path.module)}/hosts.cfg"
 }
