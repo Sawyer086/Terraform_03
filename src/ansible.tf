2081d57c6c389f65d81f9bf60f7fc11e7200da6c
Sawyer086 <145470451+Sawyer086@users.noreply.github.com>
2025-04-03 19:55:02 +0500

    Update ansible.tf

--- src/ansible.tf
+++ src/ansible.tf
@@ -1,16 +1,6 @@
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/ansible_inventory.tpl", {
-    webservers = [
-      { name = "web-1", public_ip = module.web-1.public_ip, fqdn = module.web-1.fqdn },
-      { name = "web-2", public_ip = module.web-2.public_ip, fqdn = module.web-2.fqdn }
-    ]
-    databases = [
-      { name = "main", public_ip = module.db-main.public_ip, fqdn = module.db-main.fqdn },
-      { name = "replica", public_ip = module.db-replica.public_ip, fqdn = module.db-replica.fqdn }
-    ]
-    storage = [
-      { name = "storage", public_ip = module.storage.public_ip, fqdn = module.storage.fqdn }
-    ]
-  })
-  filename = "${path.module}/inventory.ini"
+resource “local_file” “hosts_cfg” {
+content = templatefile(“path.module/hosts.tftpl",webservers=yandex 
+compute instance.webserversdatabases=yandex 
+compute instance.databasesstorage=yandex 
+compute instance.storage)filename="{abspath(path.module)}/hosts.cfg”
 }
